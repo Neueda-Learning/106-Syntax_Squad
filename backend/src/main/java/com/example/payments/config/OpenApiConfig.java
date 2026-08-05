061cedb118499a84342b9e834d3a4fc3ac9768b9
AnirbanGhoshh <anirbang749@gmail.com>
2026-08-05 08:09:07 +0000

    add config files

---
+++ backend/src/main/java/com/example/payments/config/OpenApiConfig.java
@@ -0,0 +1,9 @@
+package com.example.payments.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Payment Processing API", version = "v1"))
+public class OpenApiConfig {}
